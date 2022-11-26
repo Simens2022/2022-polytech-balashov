8fa799447a9216f3e2b512ec1d0c55584fe54286
Simens2022 <dmitry.balashov@technosprint.ru>
2022-11-26 10:14:07 +0300

    add(03_cpp/intro): intro.cpp and cmake project

---
+++ 03_cpp/intro/intro.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::string a;
+    std::cout << "Enter your name" << std::endl;
+    std::cin >> a;
+    std::cout << "Hello, " << a << "!" << std::endl;    
+    return 0;
+}
