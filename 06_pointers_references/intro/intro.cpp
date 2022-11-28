66c4e7011290429b26ad4c0a6f2207ad0855b4a1
Simens2022 <dmitry.balashov@technosprint.ru>
2022-11-28 23:25:47 +0300

    add(06_pointers_references/intro): .cpp files and cmake project

---
+++ 06_pointers_references/intro/intro.cpp
@@ -0,0 +1,29 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+unsigned int length(const char* str) {
+    if (str == nullptr) return 0;
+    else
+    {
+        int size = 0;
+        while (*str++) size++;
+
+        return size;
+    }
+}
+
+
+int main() {
+    assert(length("a") == 1);
+    assert(length("ab") == 2);
+    assert(length("") == 0);
+    assert(length("hello world") == 11);
+    assert(length(nullptr) == 0);
+
+    return 0;
+}
