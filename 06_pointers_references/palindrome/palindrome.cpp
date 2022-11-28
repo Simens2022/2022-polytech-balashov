b963dab04df794d40f1f617d92f4d0c18373b336
Simens2022 <dmitry.balashov@technosprint.ru>
2022-11-28 23:33:01 +0300

    add(06_pointers_references/palindrome): .cpp files and cmake project

---
+++ 06_pointers_references/palindrome/palindrome.cpp
@@ -0,0 +1,60 @@
+﻿#include <cassert>
+#include <iostream>
+
+
+using namespace std;
+
+
+unsigned int length(const char* str) {
+    if (str == nullptr) return 0;
+    else {
+        int size = 0;
+        while (*str++) size++;
+        
+        return size;
+    }
+}
+
+
+bool test(const char* str) {
+    if (str == nullptr) return false;
+    else {
+        if (length(str) % 2 == 0) {
+            int a = length(str);
+            for (int i = 0; i < (a / 2); i++) {
+                if (str[a / 2 - 1 - i] == str[a / 2 + i]) continue;
+                else return false;
+            }
+
+            return true;
+        }
+        else {
+            int a = length(str);
+            for (int i = 0; i < (a / 2); i++) {
+                if (str[a / 2 - i] == str[a / 2 + i]) continue;
+                else return false;
+            }
+
+            return true;
+        }
+    }
+}
+
+
+int main()
+{
+    assert(test("a") == true);
+    assert(test("ab") == false);
+    assert(test("") == true);
+    assert(test("aboba") == true);
+    assert(test("abobaboba") == true);
+    assert(test("1234567890987654321") == true);
+    assert(test("потоп") == true);
+    assert(test(nullptr) == false);
+    assert(test("довод") == true);
+    assert(test("абввба") == true);
+    assert(test("йцукенггнекуцй") == true);
+    cout << "Success!" << endl;
+
+    return 0;
+}
